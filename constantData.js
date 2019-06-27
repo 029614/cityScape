2f36f44246e14658b6c373fba2070f727af8d039
029614 <46335776+029614@users.noreply.github.com>
2019-06-27 00:16:05 -0700

    Update constantData.js

--- constantData.js
+++ constantData.js
@@ -1,222 +1,322 @@
 //constant data objects
-const buildings = {
+let buildings = {
   //all constant building data
   houses: {
     simpleHouse: {
       name: 'Simple House',
+      type: 'house',
       description: 'Houses provide shelter for citizens. Your population cannot grow beyond available housing.',
       tier: 'simple',
       costModifier: 100,
       laborModifier: 100,
-      capacityModifier: 100
+      capacityModifier: 5,
+      numberOwned: 5,
+      residents: 0,
+      image: 'images/thumbnails/smallHouse.png'
     },
     complexHouse: {
       name: 'Complex House',
+      type: 'house',
       description: 'Houses provide shelter for citizens. Your population cannot grow beyond available housing.',
       tier: 'complex',
       costModifier: 100,
       laborModifier: 100,
-      capacityModifier: 100
+      capacityModifier: 25,
+      numberOwned: 0,
+      residents: 0,
+      image: 'images/thumbnails/.png'
     },
     advancedHouse: {
       name: 'Advanced House',
+      type: 'house',
       description: 'Houses provide shelter for citizens. Your population cannot grow beyond available housing.',
       tier: 'advanced',
       costModifier: 100,
       laborModifier: 100,
-      capacityModifier: 100
+      capacityModifier: 100,
+      numberOwned: 0,
+      residents: 0,
+      image: 'images/thumbnails/.png'
     }
   },
-  productionBuildings: {
-    farms: {
-      simpleFarm: {
-        name: 'Simple Farm',
-        description: 'Farms produce grain.',
-        tier: 'simple',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: false,
-        workerInputModifier: false,
-        outputType: 'grain',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      },
-      complexFarm: {
-        name: 'Complex Farm',
-        description: 'Farms produce grain.',
-        tier: 'complex',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: false,
-        workerInputModifier: false,
-        outputType: 'grain',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      },
-      advancedFarm: {
-        name: 'Advanced Farm',
-        description: 'Farms produce grain.',
-        tier: 'advanced',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: false,
-        workerInputModifier: false,
-        outputType: 'grain',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      }
+  farms: {
+    simpleFarm: {
+      name: 'Simple Farm',
+      type: 'industry',
+      description: 'Farms produce grain.',
+      tier: 'simple',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: false,
+      workerInputModifier: false,
+      outputType: 'grain',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 4,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'simpleFarm',
+      image: 'images/thumbnails/smallFarm.png'
     },
-    lumberMills: {
-      simpleLumberMill: {
-        name: 'Simple Lumber Mill',
-        description: 'Lumber Mills produce lumber.',
-        tier: 'simple',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: false,
-        workerInputModifier: false,
-        outputType: 'lumber',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      },
-      complexLumberMill: {
-        name: 'Complex Lumber Mill',
-        description: 'Lumber Mills produce lumber.',
-        tier: 'complex',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: false,
-        workerInputModifier: false,
-        outputType: 'lumber',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      },
-      advancedLumberMill: {
-        name: 'Advanced Lumber Mill',
-        description: 'Lumber Mills produce lumber.',
-        tier: 'advanced',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: false,
-        workerInputModifier: false,
-        outputType: 'lumber',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      }
+    complexFarm: {
+      name: 'Complex Farm',
+      type: 'industry',
+      description: 'Farms produce grain.',
+      tier: 'complex',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: false,
+      workerInputModifier: false,
+      outputType: 'grain',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'complexFarm',
+      image: 'images/thumbnails/.png'
     },
-    mines: {
-      simpleMine: {
-        name: 'Simple Mine',
-        description: 'Mines produce stone and iron.',
-        tier: 'simple',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: false,
-        workerInputModifier: false,
-        outputType: ['stone', 'iron'],
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      },
-      complexMine: {
-        name: 'Complex Mine',
-        description: 'Mines produce stone and iron.',
-        tier: 'complex',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: false,
-        workerInputModifier: false,
-        outputType: ['stone', 'iron'],
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      },
-      advancedMine: {
-        name: 'Advanced Mine',
-        description: 'Mines produce stone and iron.',
-        tier: 'advanced',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: false,
-        workerInputModifier: false,
-        outputType: ['stone', 'iron'],
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      }
+    advancedFarm: {
+      name: 'Advanced Farm',
+      type: 'industry',
+      description: 'Farms produce grain.',
+      tier: 'advanced',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: false,
+      workerInputModifier: false,
+      outputType: 'grain',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'advancedFarm',
+      image: 'images/thumbnails/.png'
+    }
+  },
+  lumberMills: {
+    simpleLumberMill: {
+      name: 'Simple Lumber Mill',
+      type: 'industry',
+      description: 'Lumber Mills produce lumber.',
+      tier: 'simple',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: false,
+      workerInputModifier: false,
+      outputType: 'lumber',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 1,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'simpleLumberMill',
+      image: 'images/thumbnails/smallMill.png'
     },
-    smiths: {
-      simpleSmith: {
-        name: 'Simple Black Smith',
-        description: 'Black Smiths produce steel from iron',
-        tier: 'simple',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: 'iron',
-        workerInputModifier: 100,
-        outputType: 'steel',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      },
-      complexSmith: {
-        name: 'Complex Black Smith',
-        description: 'Black Smiths produce steel from iron',
-        tier: 'complex',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: 'iron',
-        workerInputModifier: 100,
-        outputType: 'steel',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      },
-      advancedSmith: {
-        name: 'Advanced Black Smith',
-        description: 'Black Smiths produce steel from iron',
-        tier: 'advanced',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: 'iron',
-        workerInputModifier: 100,
-        outputType: 'steel',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      }
+    complexLumberMill: {
+      name: 'Complex Lumber Mill',
+      type: 'industry',
+      description: 'Lumber Mills produce lumber.',
+      tier: 'complex',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: false,
+      workerInputModifier: false,
+      outputType: 'lumber',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'complexLumberMill',
+      image: 'images/thumbnails/.png'
     },
-    bakeries: {
-      simpleBakery: {
-        name: 'Simple Bakery',
-        description: 'Bakeries produce bread from grain',
-        tier: 'simple',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: 'grain',
-        workerInputModifier: 100,
-        outputType: 'bread',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      },
-      complexBakery: {
-        name: 'Complex Bakery',
-        description: 'Bakeries produce bread from grain',
-        tier: 'complex',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: 'grain',
-        workerInputModifier: 100,
-        outputType: 'bread',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      },
-      advancedBakery: {
-        name: 'Advanced Bakery',
-        description: 'Bakeries produce bread from grain',
-        tier: 'advanced',
-        costModifier: 100,
-        laborModifier: 100,
-        inputType: 'grain',
-        workerInputModifier: 100,
-        outputType: 'bread',
-        workerOutputModifier: 100,
-        numberOfWorkersModifier: 100
-      }
+    advancedLumberMill: {
+      name: 'Advanced Lumber Mill',
+      type: 'industry',
+      description: 'Lumber Mills produce lumber.',
+      tier: 'advanced',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: false,
+      workerInputModifier: false,
+      outputType: 'lumber',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'advancedLumberMill',
+      image: 'images/thumbnails/.png'
+    }
+  },
+  mines: {
+    simpleMine: {
+      name: 'Simple Mine',
+      type: 'industry',
+      description: 'Mines produce stone and iron.',
+      tier: 'simple',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: false,
+      workerInputModifier: false,
+      outputType: ['stone', 'iron'],
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 1,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'simpleMine',
+      image: 'images/thumbnails/smallMine.png'
+    },
+    complexMine: {
+      name: 'Complex Mine',
+      type: 'industry',
+      description: 'Mines produce stone and iron.',
+      tier: 'complex',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: false,
+      workerInputModifier: false,
+      outputType: ['stone', 'iron'],
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'complexMine',
+      image: 'images/thumbnails/.png'
+    },
+    advancedMine: {
+      name: 'Advanced Mine',
+      type: 'industry',
+      description: 'Mines produce stone and iron.',
+      tier: 'advanced',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: false,
+      workerInputModifier: false,
+      outputType: ['stone', 'iron'],
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'advancedMine',
+      image: 'images/thumbnails/.png'
+    }
+  },
+  smiths: {
+    simpleSmith: {
+      name: 'Simple Black Smith',
+      type: 'industry',
+      description: 'Black Smiths produce steel from iron',
+      tier: 'simple',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: 'iron',
+      workerInputModifier: 100,
+      outputType: 'steel',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'simpleSmith',
+      image: 'images/thumbnails/.png'
+    },
+    complexSmith: {
+      name: 'Complex Black Smith',
+      type: 'industry',
+      description: 'Black Smiths produce steel from iron',
+      tier: 'complex',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: 'iron',
+      workerInputModifier: 100,
+      outputType: 'steel',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'complexSmith',
+      image: 'images/thumbnails/.png'
+    },
+    advancedSmith: {
+      name: 'Advanced Black Smith',
+      type: 'industry',
+      description: 'Black Smiths produce steel from iron',
+      tier: 'advanced',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: 'iron',
+      workerInputModifier: 100,
+      outputType: 'steel',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'advancedSmith',
+      image: 'images/thumbnails/.png'
+    }
+  },
+  bakeries: {
+    simpleBakery: {
+      name: 'Simple Bakery',
+      type: 'industry',
+      description: 'Bakeries produce bread from grain',
+      tier: 'simple',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: 'grain',
+      workerInputModifier: 100,
+      outputType: 'bread',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'simpleBakery',
+      image: 'images/thumbnails/.png'
+    },
+    complexBakery: {
+      name: 'Complex Bakery',
+      type: 'industry',
+      description: 'Bakeries produce bread from grain',
+      tier: 'complex',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: 'grain',
+      workerInputModifier: 100,
+      outputType: 'bread',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'complexBakery',
+      image: 'images/thumbnails/.png'
+    },
+    advancedBakery: {
+      name: 'Advanced Bakery',
+      type: 'industry',
+      description: 'Bakeries produce bread from grain',
+      tier: 'advanced',
+      costModifier: 100,
+      laborModifier: 100,
+      inputType: 'grain',
+      workerInputModifier: 100,
+      outputType: 'bread',
+      workerOutputModifier: 100,
+      numberOfWorkersModifier: 100,
+      numberOwned: 0,
+      workerCapacity: 4,
+      workers: 0,
+      id: 'advancedBakery',
+      image: 'images/thumbnails/.png'
     }
   }
 };
@@ -307,42 +407,48 @@ const resources = { //contains constant data about resources
     type: ['food', 'ingredient', 'resource'], //type of object, may be more then one.
     feeds: 1, //how many people one unit feeds.
     produces: 'bread', //what resource this ingredient makes.
-    productionQuantity: 0.2 //how much of a resource this ingredient makes.
+    productionQuantity: 0.2, //how much of a resource this ingredient makes.
+    imageSource: 'images/icons/grainLogo.png'
   },
   bread: {
     name: 'bread',
     type: 'food', //type of object, may be more then one.
     feeds: 10, //how many people one unit feeds.
     produces: false, //what resource this ingredient makes.
-    productionQuantity: false //how much of a resource this ingredient makes.
+    productionQuantity: false, //how much of a resource this ingredient makes.
+    imageSource: 'images/icons/breadLogo.png'
   },
   lumber: {
     name: 'lumber',
     type: 'resource', //type of object, may be more then one.
     feeds: false, //how many people one unit feeds.
     produces: false, //what resource this ingredient makes.
-    productionQuantity: false //how much of a resource this ingredient makes.
+    productionQuantity: false, //how much of a resource this ingredient makes.
+    imageSource: 'images/icons/lumberLogo.png'
   },
   stone: {
     name: 'stone',
     type: 'resource', //type of object, may be more then one.
     feeds: false, //how many people one unit feeds.
     produces: false, //what resource this ingredient makes.
-    productionQuantity: false //how much of a resource this ingredient makes.
+    productionQuantity: false, //how much of a resource this ingredient makes.
+    imageSource: 'images/icons/stoneLogo.png'
   },
   iron: {
     name: 'iron',
     type: ['ingredient', 'resource'], //type of object, may be more then one.
     feeds: false, //how many people one unit feeds.
     produces: 'steel', //what resource this ingredient makes.
-    productionQuantity: .25 //how much of a resource this ingredient makes.
+    productionQuantity: .25, //how much of a resource this ingredient makes.
+    imageSource: 'images/icons/ironLogo.png'
   },
   steel: {
     name: 'steel',
     type: 'resource', //type of object, may be more then one.
     feeds: false, //how many people one unit feeds.
     produces: false, //what resource this ingredient makes.
-    productionQuantity: false //how much of a resource this ingredient makes.
+    productionQuantity: false, //how much of a resource this ingredient makes.
+    imageSource: 'images/icons/steelLogo.png'
   }
 };
 
